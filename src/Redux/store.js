34134ff149db59a8d5d8f39f48438d41f22c9f3b
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-06-19 22:45:01 +0300

    edit code

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,8 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import { contactsReducer } from './contactsSlice';
-import { filterReducer } from './filterSlice';
 import storage from 'redux-persist/lib/storage';
 import { persistStore, persistReducer } from 'redux-persist';
+import { contactsReducer } from './contactsSlice';
+import { filterReducer } from './filterSlice';
 
 const persistConfig = {
   key: 'root',
